dcf7f185645fa675cfdd8c176df15b58945c81f6
ZoRo <155851575+ZoRoxDed@users.noreply.github.com>
2024-09-30 04:06:36 +0000

    add config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
+    CLIENT_ID: "1290073496440803419", // Discord Application ID
     COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
     PRODUCT_NAME: "Just Another Generic Roguelike", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
